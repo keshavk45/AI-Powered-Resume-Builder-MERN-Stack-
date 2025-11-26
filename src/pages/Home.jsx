b4a043097c7d017a135aaf1a1e20d4f5c8e36b71
keshavk45 <keshavk0682@gmail.com>
2025-11-27 04:43:21 +0530

    Home Page UI completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Banner from '../components/Banner.jsx';
 import Hero from '../components/Hero.jsx';
 import Features from '../components/Features.jsx';
+import Testimonial from '../components/Testimonial.jsx';
+import CalltoAction from '../components/CalltoAction.jsx';
+import Footer from '../components/Footer.jsx';
 
 const Home = () => {
   return (
@@ -9,6 +12,9 @@ const Home = () => {
         <Banner />
         <Hero />
         <Features />
+        <Testimonial />
+        <CalltoAction />
+        <Footer />
         
     </div>
   )
