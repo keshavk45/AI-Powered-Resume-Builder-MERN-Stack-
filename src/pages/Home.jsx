1b0cf559ca50fc8158fa46dc0e243e6094f544b3
keshavk45 <keshavk0682@gmail.com>
2025-11-26 23:14:19 +0530

    Day 1 progress - basic setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from '../components/Banner.jsx';
+import Hero from '../components/Hero.jsx';
+import Features from '../components/Features.jsx';
+
+const Home = () => {
+  return (
+    <div>
+        <Banner />
+        <Hero />
+        <Features />
+        
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/pages/preview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const preview = () => {
+  return (
+    <div>
+      Preview Page
+    </div>
+  );
+};
+
+export default preview;
