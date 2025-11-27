c3594078ddcfaa6cd9b0917a2e5f90955b01f026
keshavk45 <keshavk0682@gmail.com>
2025-11-27 23:06:16 +0530

    Completed Login Page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    const user = {name: 'John Doe'};
+  return (
+    <div className='shadow bg-white'>
+        <nav className=''>
+
+        </nav>
+
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Banner from '../components/Banner.jsx';
-import Hero from '../components/Hero.jsx';
-import Features from '../components/Features.jsx';
-import Testimonial from '../components/Testimonial.jsx';
-import CalltoAction from '../components/CalltoAction.jsx';
-import Footer from '../components/Footer.jsx';
+import Banner from '../components/Home/Banner.jsx';
+import Hero from '../components/Home/Hero.jsx';
+import Features from '../components/Home/Features.jsx';
+import Testimonial from '../components/Home/Testimonial.jsx';
+import CalltoAction from '../components/Home/CalltoAction.jsx';
+import Footer from '../components/Home/Footer.jsx';
 
 const Home = () => {
   return (
