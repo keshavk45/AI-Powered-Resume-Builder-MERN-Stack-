9dc78dd423e608beda40c0b597c0012c4238f2b7
keshavk45 <keshavk0682@gmail.com>
2025-12-29 15:15:17 +0530

    Add structured JSON format for resume data extraction in uploadResume controller

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -75,7 +75,49 @@ export const uploadResume = async (req , res) =>{
         }
 
         const systemPrompt = "You are an expert AI Agent to extract data from resume"
-        const userPrompt = `Extract data from this resume: ${resumeText}`
+        const userPrompt = `Extract data from this resume: ${resumeText}
+        Provide data in the following JSON format with no additional text before or after:
+        
+        {
+            professional_summary: { type: String, default: '' },
+  skills: [{ type: String }],
+  personal_info: {
+    image: { type: String, default: '' },
+    full_name: { type: String, default: '' },
+    profession: { type: String, default: '' },
+    email: { type: String, default: '' },
+    phone: { type: String, default: '' },
+    location: { type: String, default: '' },
+    linkedin: { type: String, default: '' },
+    location: { type: String, default: '' }, 
+  },
+  work_experience: [
+    {
+      company: { type: String }, 
+      position: { type: String },
+      start_date: { type: String },
+      end_date: { type: String },
+      description: { type: String },
+      is_current: { type: Boolean },
+    }
+  ],
+  project: [
+    {
+      name: { type: String },
+      type: { type: String },
+      description: { type: String },
+    }
+  ],
+  education: [
+    {
+      institution: { type: String },
+      degree: { type: String },
+      field: { type: String },
+      graduation_date: { type: String },
+      gpa: { type: String },
+    }
+  ],}
+        `;
         const response = await openai.chat.completions.create({
             model: process.env.OPENAI_MODEL,
             messages: [
