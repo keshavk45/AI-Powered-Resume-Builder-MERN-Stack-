c9757dcabf920c5d839084a66d29371d9db91995
keshavk45 <keshavk0682@gmail.com>
2025-12-29 15:09:24 +0530

    Add uploadResume controller to extract and save resume data

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,7 +1,9 @@
-
+import Resume from "../models/Resume.js";
 
 // controller for enhancing a resume's professional summary
 // POST: /api/ai/enhance-pro-sum
+
+
 
 export const enhanceProfessionalSummary = async (req , res) =>{
     try{
@@ -58,3 +60,48 @@ export const enhanceJobDescription = async (req , res) =>{
         return res.status(400).json({message: error.message})
         }
     }
+
+    //controller for uploading a resume to the database
+// POST: /api/ai/upload-resume
+
+export const uploadResume = async (req , res) =>{
+    try{
+        
+        const {resumeText , title} = req.body;
+        const userId = req.userId;
+
+        if(!resumeText){
+            return res.status(400).json({message : 'Missing required fields'})
+        }
+
+        const systemPrompt = "You are an expert AI Agent to extract data from resume"
+        const userPrompt = `Extract data from this resume: ${resumeText}`
+        const response = await openai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+        { role: "system", 
+            content: systemPrompt },
+        {
+            role: "user",
+            content: userPrompt,
+        },
+    ],
+    response_format: {type : "json_object" },
+        })
+
+        const extractedData = response.choices[0].message.content;
+        const parsedData = JSON.parse(extractedData);
+        const newResume = await Resume.create({
+            userId,
+            title,
+            ...parsedData
+        })
+        res.json({resumeId: newResume._id})
+
+        const enhancedContent = response.choices[0].message.content
+        return res.status(200).json({enhancedContent})
+    }    catch (error){
+        return res.status(400).json({message: error.message})
+        }
+    }
+
